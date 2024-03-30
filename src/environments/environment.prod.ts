b61aa5391c2c816532c158a091190c15c0cad330
Aliaksei Zviazhynski <alex.zwezh@gmail.com>
2024-03-30 21:22:10 +0100

    Fix git ignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from '@appTypes';
+
+export const environment: Environment = {
+  apiUrl: 'https://cinema-catalogue-be.onrender.com/api',
+  kinopoiskToken: 'T11347S-7JJMGDN-GPJT8YX-2KPCPMQ',
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+import { Environment } from '@appTypes';
+
+export const environment: Environment = {
+  apiUrl: 'http://localhost:4200/api',
+  kinopoiskToken: 'ZZT1GA1-PD4MPDG-MJWTJ90-G7Q3BKJ',
+  production: false
+  // kinopoiskToken: 'T11347S-7JJMGDN-GPJT8YX-2KPCPMQ',
+};
